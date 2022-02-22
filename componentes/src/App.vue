f7ecb551d272942638140ff4cc20c9b6ca45f2fd
andres rojas <andath1@hotmail.com>
2022-02-22 16:31:27 -0300

    Componentes

---
+++ componentes/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <HelloWorld  />
+</template>
+
+<style>
+#app {
+  background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  text-align: left;
+}
+</style>
---
+++ componentes/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+name:"HelloWorld",
+  data() {
+    return {
+      personajes: [{
+                    nombre: "Rick Sanchez ",
+                    imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg "
+                }, {
+                    nombre: "Morty Smith ",
+                    imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg "
+                }, {
+                    nombre: "Summer Smith ",
+                    imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg "
+                }, {
+                    nombre: "Beth Smith ",
+                    imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg "
+                }, {
+                    nombre: "Jerry Smith ",
+                    imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg "
+                }, ]
+    }
+  }
+}
+</script>
+
+<template>
+<div class="animados">
+ <ul>
+            <li v-for="(personaje, index) in personajes" :key="index"><br>
+                <img :src="personaje.imagen" alt="imagen" class="m-3"> {{personaje.nombre}}
+            </li>
+        </ul>
+</div>
+</template>
+
+<style scoped>
+
+img{
+  text-align: left;
+  border-radius: 50%;
+  width: 200px;
+}
+
+ul,
+li {
+    list-style: none;
+}
+.animados {
+    color: green;
+    font-size: 25px;
+}
+</style>
